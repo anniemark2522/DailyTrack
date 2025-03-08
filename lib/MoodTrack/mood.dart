450aaa3328c3798b1aa79c75608c93effb559901
Veeraya Lekchaoum <114289600+VaLin4637@users.noreply.github.com>
2025-03-08 18:12:08 +0700

    2

--- lib/MoodTrack/mood.dart
+++ lib/MoodTrack/mood.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_anne/MoodTrack/addMood.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:intl/intl.dart';
 
 class MoodPage extends StatefulWidget {
   const MoodPage({super.key});
@@ -12,33 +11,13 @@ class MoodPage extends StatefulWidget {
 
 class _MoodPageState extends State<MoodPage> {
   DateTime _focusedDay = DateTime.now();
-  // DateTime? _selectedDay;
+  // ตัวแปรที่เก็บค่าการเลือก mood
+  List<int?> selectedMoods = [null, null, null];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEFF9F0),
-      // appBar: AppBar(
-      //     backgroundColor: Colors.white,
-      //     title: Row(
-      //       children: [
-      //         CircleAvatar(
-      //           backgroundColor: Colors.grey[300],
-      //           child: Icon(
-      //             Icons.person,
-      //             color: Colors.white,
-      //           ),
-      //         ),
-      //         SizedBox(
-      //           width: 10,
-      //         ),
-      //         Text(
-      //           "${_focusedDay.month}" "/" "${_focusedDay.year}",
-      //           style: TextStyle(color: Colors.black, fontSize: 18),
-      //         ),
-      //         Icon(Icons.arrow_drop_down, color: Colors.black),
-      //       ],
-      //     )),
       body: Column(
         children: [
           TableCalendar(
@@ -46,29 +25,56 @@ class _MoodPageState extends State<MoodPage> {
             lastDay: DateTime.utc(2030, 12, 31),
             calendarFormat: CalendarFormat.month,
             focusedDay: _focusedDay,
-            // selectedDayPredicate: (day) {
-            //   return isSameDay(_selectedDay, day);
-            // },
-            // onDaySelected: (selectedDay, focusedDay) {
-            //   setState(() {
-            //     _selectedDay = selectedDay;
-            //     _focusedDay = focusedDay;
-            //   });
-            // },
             calendarStyle: CalendarStyle(
               todayDecoration: BoxDecoration(
-                color: Colors.orange,
+                color: Color(0XFFB3E680),
                 shape: BoxShape.circle,
               ),
-              // selectedDecoration: BoxDecoration(
-              //   color: Colors.red,
-              //   shape: BoxShape.circle,
-              // ),
-              defaultTextStyle: TextStyle(color: Colors.black),
+              todayTextStyle: TextStyle(
+                color: Colors.green,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Color(0XFFFEB0B9),
+                shape: BoxShape.circle,
+              ),
+              defaultTextStyle: TextStyle(
+                color: Color(0xFF534684),
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+              weekendTextStyle: TextStyle(
+                color: Colors.red,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+              selectedTextStyle: TextStyle(
+                color: Color(0xFF534684),
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             headerStyle: HeaderStyle(
               formatButtonVisible: false,
               titleCentered: true,
+              titleTextStyle: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            daysOfWeekStyle: DaysOfWeekStyle(
+              weekdayStyle: TextStyle(
+                color: Colors.green,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+              weekendStyle: TextStyle(
+                color: Colors.red,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ],
